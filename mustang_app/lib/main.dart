dee4ba7501922ce47a8a677b607d43c2158ca114
KatiaBravo <katia@alexbravo.com>
2021-01-12 13:40:14 -0800

    Change if-else chain to switch statement

--- mustang_app/lib/main.dart
+++ mustang_app/lib/main.dart
@@ -11,87 +11,89 @@ class MyApp extends StatelessWidget {
 
   Route<dynamic> _onGenerateRoute(RouteSettings settings) {
     Map<String, dynamic> args = settings.arguments;
-    if (settings.name == Calendar.route) {
-      return MaterialPageRoute(
-        builder: (context) => Calendar(),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == Scouter.route) {
-      return MaterialPageRoute(
-        builder: (context) => Scouter(),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == AutonScouter.route) {
-      return MaterialPageRoute(
-        builder: (context) => AutonScouter(
-          teamNumber: args['teamNumber'],
-          matchNumber: args['matchNumber'],
-        ),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == TeleopScouter.route) {
-      return MaterialPageRoute(
-        builder: (context) => TeleopScouter(
-          teamNumber: args['teamNumber'],
-          matchNumber: args['matchNumber'],
-        ),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == EndgameScouter.route) {
-      return MaterialPageRoute(
-        builder: (context) => EndgameScouter(
-          teamNumber: args['teamNumber'],
-          matchNumber: args['matchNumber'],
-        ),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == MatchEndScouter.route) {
-      return MaterialPageRoute(
-        builder: (context) => MatchEndScouter(
-          teamNumber: args['teamNumber'],
-          matchNumber: args['matchNumber'],
-        ),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == PostScouter.route) {
-      return MaterialPageRoute(
-        builder: (context) => PostScouter(),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == MapAnalysisDisplay.route) {
-      return MaterialPageRoute(
-        builder: (context) => MapAnalysisDisplay(
-          teamNumber: args['teamNumber'],
-        ),
-        settings: rotationSettings(settings, ScreenOrientation.landscapeOnly),
-      );
-    } else if (settings.name == SketchPage.route) {
-      return MaterialPageRoute(
-        builder: (context) => SketchPage(),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == SearchPage.route) {
-      return MaterialPageRoute(
-        builder: (context) => SearchPage(),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == TeamInfoDisplay.route) {
-      return MaterialPageRoute(
-        builder: (context) => TeamInfoDisplay(teamNumber: args['teamNumber']),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == PitScouter.route) {
-      return MaterialPageRoute(
-        builder: (context) => PitScouter(teamNumber: args['teamNumber']),
-        settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
-      );
-    } else if (settings.name == MapScouting.route) {
-      return MaterialPageRoute(
-        builder: (context) => MapScouting(),
-        settings: rotationSettings(settings, ScreenOrientation.landscapeOnly),
-      );
+    switch(settings.name) {
+      case Calendar.route:
+        return MaterialPageRoute(
+          builder: (context) => Calendar(),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case Scouter.route:
+        return MaterialPageRoute(
+          builder: (context) => Scouter(),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case AutonScouter.route:
+        return MaterialPageRoute(
+          builder: (context) => AutonScouter(
+            teamNumber: args['teamNumber'],
+            matchNumber: args['matchNumber'],
+          ),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case TeleopScouter.route:
+        return MaterialPageRoute(
+          builder: (context) => TeleopScouter(
+            teamNumber: args['teamNumber'],
+            matchNumber: args['matchNumber'],
+          ),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case EndgameScouter.route:
+        return MaterialPageRoute(
+          builder: (context) => EndgameScouter(
+            teamNumber: args['teamNumber'],
+            matchNumber: args['matchNumber'],
+          ),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case MatchEndScouter.route:
+        return MaterialPageRoute(
+          builder: (context) => MatchEndScouter(
+            teamNumber: args['teamNumber'],
+            matchNumber: args['matchNumber'],
+          ),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case PostScouter.route:
+        return MaterialPageRoute(
+          builder: (context) => PostScouter(),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case MapAnalysisDisplay.route:
+        return MaterialPageRoute(
+          builder: (context) => MapAnalysisDisplay(
+            teamNumber: args['teamNumber'],
+          ),
+          settings: rotationSettings(settings, ScreenOrientation.landscapeOnly),
+        );
+      case SketchPage.route:
+        return MaterialPageRoute(
+          builder: (context) => SketchPage(),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case SearchPage.route:
+        return MaterialPageRoute(
+          builder: (context) => SearchPage(),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case TeamInfoDisplay.route:
+        return MaterialPageRoute(
+          builder: (context) => TeamInfoDisplay(teamNumber: args['teamNumber']),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case PitScouter.route:
+        return MaterialPageRoute(
+          builder: (context) => PitScouter(teamNumber: args['teamNumber']),
+          settings: rotationSettings(settings, ScreenOrientation.portraitOnly),
+        );
+      case MapScouting.route:
+        return MaterialPageRoute(
+          builder: (context) => MapScouting(),
+          settings: rotationSettings(settings, ScreenOrientation.landscapeOnly),
+        );
+      default:
+        return null;
     }
-    return null;
   }
 
   @override
