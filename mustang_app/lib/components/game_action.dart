f1886b951e70d821c6b25223f47178dcfcd1e37f
KatiaBravo <katia@alexbravo.com>
2021-01-26 09:50:07 -0800

    Refactor GameAction (turns out variables need to use camel case, file names are the only ones with underscores)

--- mustang_app/lib/components/game_action.dart
+++ mustang_app/lib/components/game_action.dart
@@ -1,14 +1,35 @@
-enum ActionType {FOUL_REG, FOUL_TECH, FOUL_YELLOW, FOUL_RED, FOUL_DISABLED, FOUL_DISQUAL,
-                SHOT_LOW, SHOT_OUTER, SHOT_INNER, MISSED_LOW, MISSED_OUTER,
-                OTHER_CLIMB, OTHER_CLIMB_MISS, OTHER_WHEEL_POSITION, OTHER_WHEEL_COLOR,
-                PREV_SHOT, PREV_INTAKE, PUSH}
-class GameAction{
+class GameAction {
   //end row, end column, push time are only for push, will be null most of time
-  double seconds_elapsed, x, y, end_x, end_y, push_time;
-  ActionType action;
-  GameAction(this.action, this.seconds_elapsed, this.x, this.y, );
-  GameAction.push(this.seconds_elapsed, this.x, this.y, this.end_x, this.end_y, this.push_time){
-    action = ActionType.PUSH;
+  double _secondsElapsed, _x, _y, _endX, _endY, _pushTime;
+  ActionType _action;
+  GameAction(
+    this._action,
+    this._secondsElapsed,
+    this._x,
+    this._y,
+  );
+  GameAction.push(this._secondsElapsed, _x, _y, _endX, _endY, _pushTime) {
+    _action = ActionType.PUSH;
   }
+}
 
+enum ActionType {
+  FOUL_REG,
+  FOUL_TECH,
+  FOUL_YELLOW,
+  FOUL_RED,
+  FOUL_DISABLED,
+  FOUL_DISQUAL,
+  SHOT_LOW,
+  SHOT_OUTER,
+  SHOT_INNER,
+  MISSED_LOW,
+  MISSED_OUTER,
+  OTHER_CLIMB,
+  OTHER_CLIMB_MISS,
+  OTHER_WHEEL_POSITION,
+  OTHER_WHEEL_COLOR,
+  PREV_SHOT,
+  PREV_INTAKE,
+  PUSH
 }
