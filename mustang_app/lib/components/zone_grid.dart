c3f5bd82a6ecf790c77606f7434cdc880e45f973
Arnav Kulkarni <arnavyk@gmail.com>
2021-01-28 00:49:21 -0800

    added zone select

--- mustang_app/lib/components/zone_grid.dart
+++ mustang_app/lib/components/zone_grid.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:mustang_app/constants/constants.dart';
 
-class ZoneGrid extends StatelessWidget {
+class ZoneGrid extends StatefulWidget {
   Function(int x, int y) _onTap;
-
   ZoneGrid(this._onTap);
+
+  @override
+  _ZoneGridState createState() => _ZoneGridState(_onTap);
+}
+
+class _ZoneGridState extends State<ZoneGrid> {
+  int _selectedX, _selectedY;
+  bool _hasSelected;
+
+  Function(int x, int y) _onTap;
+  _ZoneGridState(this._onTap);
+
+  @override
+  void initState() {
+    super.initState();
+    _hasSelected = false;
+  }
+
+  int get x {
+    return _selectedX;
+  }
+
+  int get y {
+    return _selectedY;
+  }
 
   List<TableRow> _getTableContents(double width, double height) {
     List<TableRow> tableRows = [];
@@ -20,9 +44,27 @@ class ZoneGrid extends StatelessWidget {
               behavior: HitTestBehavior.translucent,
               onTap: () {
                 _onTap(j, i);
-                print("($j,$i)");
+                setState(() {
+                  _hasSelected =
+                      _selectedX != j || _selectedY != i ? true : !_hasSelected;
+                  _selectedX = j;
+                  _selectedY = i;
+                });
               },
-              child: Container(
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeOut,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: _hasSelected && _selectedX == j && _selectedY == i
+                        ? LinearGradient(
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                            colors: [
+                                Colors.blue.shade100.withOpacity(0.9),
+                                Colors.green.shade100.withOpacity(0.9),
+                              ])
+                        : null),
                 key: Key("($j,$i)"),
                 height: cellHeight,
                 width: cellWidth,
