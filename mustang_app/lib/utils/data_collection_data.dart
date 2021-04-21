516c051ae0e058c14b8dc31464e54b63f06a7390
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-04-21 14:26:05 -0700

    Fixed issue where if accuracy was 100%, it would show 0%

--- mustang_app/lib/utils/data_collection_data.dart
+++ mustang_app/lib/utils/data_collection_data.dart
@@ -102,10 +102,16 @@ class DataCollectionMatchData {
 
     try {
       percentageScored = row[3] as double;
+      if (matchName == "Quals 17") {
+        print("----- Got ${row[3]} as data");
+      }
     } catch (e) {
-      print("Invalid input, defaulting to 0% scored");
-      percentageScored = 0;
-      failed_data++;
+      if (row[3] == 1) {
+        percentageScored = 1;
+      } else {
+        percentageScored = 0;
+        failed_data++;
+      }
     }
 
     try {
