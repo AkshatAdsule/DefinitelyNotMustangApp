e8379da2e5b9116ec49cd1149199aa41b925ed56
Elise Vambenepe <evambenepe621@student.fuhsd.org>
2021-08-13 10:06:11 -0700

    *now* it runs

--- mustang_app/lib/components/analysis/analysis.dart
+++ mustang_app/lib/components/analysis/analysis.dart
@@ -2,4 +2,4 @@ export 'data_display_text.dart';
 export 'game_replay_key.dart';
 export 'map_analysis_key.dart';
 export 'map_analysis_text.dart';
-export 'written_analysis_display.dart';
+// export 'written_analysis_display.dart';
