ac0a1fdcdada24995d614b7ea696c0f9a489ce3f
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-12-19 13:42:06 -0800

    generalize climb actions

---
+++ mustang_app/lib/constants/game_actions.dart
@@ -0,0 +1,41 @@
+import 'package:mustang_app/models/game_action.dart';
+import 'package:mustang_app/pages/pages.dart';
+
+class RobotAction {
+  String text;
+  Function onPress;
+
+  RobotAction(String text, Function onPress) {
+    this.onPress = onPress;
+    this.text = text;
+  }
+}
+
+class GameActions {
+  static List<RobotAction> climbActions = [
+    RobotAction(
+      "No Park",
+      (MapScoutingState currentState) {
+        currentState.addClimb(ActionType.OTHER_CLIMB_MISS);
+      },
+    ),
+    RobotAction(
+      "Parked",
+      (MapScoutingState currentState) {
+        currentState.addClimb(ActionType.OTHER_PARKED);
+      },
+    ),
+    RobotAction(
+      "Climbed",
+      (MapScoutingState currentState) {
+        currentState.addClimb(ActionType.OTHER_CLIMB);
+      },
+    ),
+    RobotAction(
+      "Levelled",
+      (MapScoutingState currentState) {
+        currentState.addClimb(ActionType.OTHER_LEVELLED);
+      },
+    ),
+  ];
+}
