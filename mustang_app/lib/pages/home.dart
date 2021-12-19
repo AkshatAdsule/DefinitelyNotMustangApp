eb0d7bf4cf44e607d98b416a3c19db3c74d53278
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-12-19 12:58:57 -0800

    update homescreen

--- mustang_app/lib/pages/home.dart
+++ mustang_app/lib/pages/home.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:mustang_app/components/shared/screen.dart';
-import 'package:mustang_app/constants/constants.dart';
-import 'package:mustang_app/constants/legacy.dart';
+import 'package:mustang_app/pages/pages.dart';
 
 class Home extends StatefulWidget {
   static const String route = '/';
   @override
   State<StatefulWidget> createState() {
     return HomeState();
+  }
+}
+
+class NavCard extends StatelessWidget {
+  final String route, title;
+  final IconData icon;
+
+  const NavCard(
+      {@required this.route, @required this.title, @required this.icon});
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              )
+            ],
+          ),
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, route);
+        },
+        enableFeedback: true,
+      ),
+      color: Colors.green,
+      borderRadius: BorderRadius.circular(10),
+      elevation: 10,
+    );
   }
 }
 
@@ -18,66 +65,130 @@ class HomeState extends State<Home> {
       title: 'Home',
       left: false,
       right: false,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              child: Text(
-                "U R Welcome 💚",
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Center(
+            Expanded(
+              flex: 2,
               child: Container(
-                margin: EdgeInsets.only(top: 5),
-                child: Column(
+                child: Row(
                   children: [
-                    Text(
-                      "Created by:",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 18,
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        "The Mustang Alliance",
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                    ...Legacy.creators
-                        .map(
-                          (e) => Container(
-                            margin: EdgeInsets.only(top: 5),
-                            child: Text(
-                              e.toString(),
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.green,
-                              ),
-                            ),
-                          ),
-                        )
-                        .toList(),
-                    SizedBox(
-                      height: 100,
-                    ),
-                    Container(
-                      child: Column(
-                        children: [
-                          Text(
-                            "Pre-Event data version: ${Constants.DATA_ANALYSIS_DATA_VERSION}",
-                          ),
-                          Text(
-                            "Data-Collection data version: ${Constants.DATA_COLLECTION_DATA_VERSION}",
-                          ),
-                        ],
+                    Expanded(
+                      flex: 1,
+                      child: Image.asset(
+                        "assets/logo.png",
                       ),
                     ),
                   ],
                 ),
               ),
             ),
+            // Expanded(
+            //   flex: 2,
+            //   child: Image.asset(
+            //     "assets/logo.png",
+            //   ),
+            // ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: NavCard(
+                            route: Scouter.route,
+                            title: "Match Scouting",
+                            icon: Icons.list,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: NavCard(
+                            route: SearchPage.route,
+                            title: "Data Analysis",
+                            icon: Icons.search,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: NavCard(
+                            route: InputScreen.route,
+                            title: "Team Analysis",
+                            icon: Icons.stacked_line_chart,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: NavCard(
+                            route: SketchPage.route,
+                            title: "Match Planning",
+                            icon: Icons.edit,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Expanded(
+            //   flex: 3,
+            //   child: GridView.count(
+            //     crossAxisCount: 2,
+            //     crossAxisSpacing: 10,
+            //     mainAxisSpacing: 10,
+            //     children: const [
+            // NavCard(
+            //   route: "",
+            //   title: "Match Scouting",
+            //   icon: Icons.list,
+            // ),
+            // NavCard(
+            //   route: "",
+            //   title: "Data Analysis",
+            //   icon: Icons.search,
+            // ),
+            // NavCard(
+            //   route: "",
+            //   title: "Team Analysis",
+            //   icon: Icons.stacked_line_chart,
+            // ),
+            // NavCard(
+            //   route: "",
+            //   title: "Match Planing",
+            //   icon: Icons.edit,
+            // ),
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
